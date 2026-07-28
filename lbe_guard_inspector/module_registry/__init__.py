d9a69a250487bac4f5676ca3b1c3370deef60551
Letterblack <pravesh.pandey@letterblack.ae>
2026-07-28 04:25:03 +0400

    feat(registry): expose registry public API

---
+++ lbe_guard_inspector/module_registry/__init__.py
@@ -0,0 +1,25 @@
+from .models import (
+    LifecycleReceipt,
+    LiveModuleRecord,
+    ModuleActivity,
+    ModuleDeclaration,
+    ModuleState,
+    ModuleType,
+    ReceiptType,
+    RegistryDefect,
+    RegistryDefectCode,
+)
+from .store import ModuleRegistry
+
+__all__ = [
+    "LifecycleReceipt",
+    "LiveModuleRecord",
+    "ModuleActivity",
+    "ModuleDeclaration",
+    "ModuleRegistry",
+    "ModuleState",
+    "ModuleType",
+    "ReceiptType",
+    "RegistryDefect",
+    "RegistryDefectCode",
+]
