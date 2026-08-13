483dfeb289b293ad415cf0e4d4a998e36dd5ce17
Letterblack <pravesh.pandey@letterblack.ae>
2026-08-13 14:04:09 +0400

    fix: await asynchronous public installer

--- npm/bin/lbe.js
+++ npm/bin/lbe.js
@@ -3,4 +3,7 @@
 
 const { runLauncher } = require("../lib/launcher");
 
-runLauncher(process.argv.slice(2));
+runLauncher(process.argv.slice(2)).catch((error) => {
+  process.stderr.write(`lbe installer: ${error.message}\n`);
+  process.exitCode = 2;
+});
